f8e48f0774eb339a8591be00ee02784ca20107cc
Elan Baharie and Peter Karellas <elan@>
2017-04-06 15:07:41 -0700

    make roll functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,50 @@
-function Game(name, roll, score, total){
-  this.name = name;
-  this.roll = roll;
-  this.score = [];
-  this.total = total;
+function Game(player1, player2, rollNum){
+  this.player1 = player1 || "jeff";
+  this.player2 = player2 || "mark";
+  this.rollNum = rollNum;
+  this.score = 0;
+  this.total = 0;
+  this.roll = function() {
+    var roll = Math.floor((Math.random()* 5)+1);
+    return roll;
+  }
+  this.holdScore = function() {
+    if(this.score === 0) {
+      console.log(this.score);
+      this.score = this.rollNum + this.score;
+      return this.score;
+    } else if (this.score > 0) {
+      console.log(this.rollNum);
+
+      return this.score + this.rollNum;
+    }
+  }
 }
 
-var roll = function () {
-  var roll = Math.floor((Math.random()* 5)+1);
-  return roll;
-}
+// var roll = function () {
+//   var roll = Math.floor((Math.random()* 5)+1);
+//   return roll;
+// }
 
-Game.prototype.holdScore = function(dieRoll) {
-
-}
+// Game.prototype.holdScore = function(dieRoll) {
+//   debugger;
+//   if(isNaN(this.score) === true) {
+//     this.score = 0;
+//     console.log(this.score);
+//     console.log(dieRoll);
+//
+//     var firstRoll = dieRoll + this.score;
+//     console.log(firstRoll);
+//
+//   } else if (this.score > 0) {
+//
+//     console.log("here");
+//     console.log(this.score + dieRoll);
+//
+//     return this.score + dieRoll;
+//   }
+//
+// }
 
 
 Game.prototype.dice = function () {
@@ -74,12 +106,9 @@ $(document).ready(function(){
 
   $("#rollPlayerDie").click(function(event) {
     event.preventDefault();
-    var sngleRoll = roll();
-    console.log(sngleRoll);
-    var tempHold = newGamePlayerOne.score;
-    console.log(tempHold);
-    newGamePlayerOne.score = sngleRoll + tempHold;
-    console.log(newGamePlayerOne.score );
+    newGamePlayerOne.rollNum = newGamePlayerOne.roll();
+    var test = newGamePlayerOne.holdScore();
+    console.log(test);
   });
   // $("#formone").submit(function(event) {
   //   event.preventDefault();
