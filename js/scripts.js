fd6f8f7c1617928520c5a27d11706a0d2945a305
Elan Baharie <0elan1@gmail.com>
2017-04-08 22:27:06 -0800

    make a barrier for the computer hold and make the player hold button work

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,22 @@ function Game(player1, player2, rollNum){
   this.rollNum = rollNum;
   this.score = 0;
   this.total = 0;
+  this.activePlayer = "";
+
   this.roll = function() {
     var roll = Math.floor((Math.random()* 5)+1);
     return roll;
   }
-  this.holdScore = function() {
-    if(this.rollNum === 1) {
+
+  this.playerTurn = function() {
+    if(this.activePlayer != "playerOne") {
+      alert("Not Your Turn");
+      return;
+    } else if (this.rollNum === 1) {
       alert("You hit one");
+      this.activePlayer = "opponent";
       return this.score = 0;
     } else if (this.score === 0) {
-      console.log(this.score);
       this.score = this.rollNum + this.score;
       return this.score;
     } else {
@@ -22,7 +28,22 @@ function Game(player1, player2, rollNum){
       return this.score;
     }
   }
+
+  this.holdSwap = function() {
+    if(this.activePlayer === "playerOne") {
+      this.total = this.score + this.total;
+      this.activePlayer = "opponent";
+      this.score = 0;
+      return;
+    } else if (this.activePlayer === "opponent") {
+      this.total = this.score + this.total;
+      this.activePlayer = "playerOne";
+      this.score = 0;
+      return;
+    }
+  }
 }
+
 
 // Game.prototype.holdScore = function(dieRoll) {
 //   debugger;
@@ -48,50 +69,61 @@ function Game(player1, player2, rollNum){
 Game.prototype.oppDice = function () {
   var pRoll = this.roll();
   console.log(pRoll);
-    if (pRoll === 1){
-      alert("Computer's Turn Ends");
-      this.score = 0;
-      return;
-    } else {
-      this.score = this.score + pRoll;
-      console.log(this.score);
-      this.oppDice();
-    }
+  if (pRoll === 1){
+    alert("Computer's Turn Ends");
+    this.score = 0;
+    this.activePlayer = "playerOne";
+    return;
+  } else if (this.score > 20) {
+    console.log("Hail");
+    this.total = this.total + this.score;
+    this.score = 0;
+    console.log(this.total);
+    this.activePlayer = "playerOne";
+    return;
+  } else {
+    this.score = this.score + pRoll;
+    console.log(this.score);
+    this.oppDice();
+  }
 }
 
 $(document).ready(function(){
 
-  var newGameOpponent = new Game();
   var newGamePlayerOne = new Game();
+  newGamePlayerOne.activePlayer = "playerOne";
 
   $("#btnOnePlyr").click(function(event) {
     event.preventDefault();
-    $("#playOne").removeClass("hide-class");
+    $("div#playOne").removeClass("hide-class");
     $("#playerOptions").hide(800);
   });
 
   $("#btnTwoPlyr").click(function(event) {
     event.preventDefault();
-    $("#playOne").removeClass("hide-class");
-    $("#playTwo").removeClass("hide-class");
+    $("div#playOne").removeClass("hide-class");
+    $("div#playTwo").removeClass("hide-class");
     $("#playerOptions").hide(800);
   });
 
   $("#formone").submit(function(event) {
     event.preventDefault();
-    newGameOpponent.oppDice();
+    newGamePlayerOne.oppDice();
   });
 
   $("#rollPlayerDie").click(function(event) {
     event.preventDefault();
+
     newGamePlayerOne.rollNum = newGamePlayerOne.roll();
-    var test = newGamePlayerOne.holdScore();
+    var test = newGamePlayerOne.playerTurn();
+
     console.log(test);
   });
-  // $("#formone").submit(function(event) {
-  //   event.preventDefault();
-  //   newGame.hold();
-  //
+
+  $("#holdOne").click(function(event) {
+    event.preventDefault();
+    newGamePlayerOne.holdSwap();
+  });
   // $("#formone").submit(function(event) {
   //   event.preventDefault();
   //   newGame.hold();
